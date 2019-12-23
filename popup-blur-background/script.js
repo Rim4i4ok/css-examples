720c1ec58caffcde58456f040b80900f7fb294a2
Rim4i4ok <rim4i4ok@gmail.com>
2019-12-23 23:21:19 +0300

    popup with blur background

---
+++ popup-blur-background/script.js
@@ -0,0 +1,7 @@
+function toggle() {
+    var container = document.getElementById('container');
+    container.classList.toggle('active');
+
+    var popup = document.getElementById('popup');
+    popup.classList.toggle('active');
+}
